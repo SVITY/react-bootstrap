e47425bf944842e679ac9e432e4a79b4b84805d0
Vitaly Solovyov <svity.developer@gmail.com>
2018-10-23 14:11:21 +0300

    prettier

--- src/disallowedProps.js
+++ src/disallowedProps.js
@@ -1,8 +1,58 @@
 const disallowedProps = {
-  a: ['activeHref', 'activeKey', 'navItem', 'eventKey', 'divider'],
-  ul: ['bsStyle', 'stacked', 'activeKey', 'justified', 'pullRight', 'pullLeft', 'bsClass', 'items', 'activePage', 'maxButtons', 'prev', 'next', 'ellipsis', 'boundaryLinks', 'first', 'last', 'buttonComponentClass'],
-  div: ['header', 'defaultExpanded', 'bsClass', 'bsStyle', 'initialValue', 'onUpdate', 'valid', 'invalid', 'dirty', 'pristine', 'touched', 'visited', 'labelClassName', 'wrapperClassName', 'componentClass', 'dropup', 'pullRight', 'bsSize', 'onToggle', 'justified', 'vertical'],
-  button: ['active', 'block', 'navItem', 'navDropdown', 'noCaret', 'useAnchor', 'bsRole'],
+  a: [
+    'activeHref', 'activeKey', 'navItem', 'eventKey', 'divider'
+  ],
+  ul: [
+    'bsStyle',
+    'stacked',
+    'activeKey',
+    'justified',
+    'pullRight',
+    'pullLeft',
+    'bsClass',
+    'items',
+    'activePage',
+    'maxButtons',
+    'prev',
+    'next',
+    'ellipsis',
+    'boundaryLinks',
+    'first',
+    'last',
+    'buttonComponentClass'
+  ],
+  div: [
+    'header',
+    'defaultExpanded',
+    'bsClass',
+    'bsStyle',
+    'initialValue',
+    'onUpdate',
+    'valid',
+    'invalid',
+    'dirty',
+    'pristine',
+    'touched',
+    'visited',
+    'labelClassName',
+    'wrapperClassName',
+    'componentClass',
+    'dropup',
+    'pullRight',
+    'bsSize',
+    'onToggle',
+    'justified',
+    'vertical'
+  ],
+  button: [
+    'active',
+    'block',
+    'navItem',
+    'navDropdown',
+    'noCaret',
+    'useAnchor',
+    'bsRole'
+  ]
 };
 
 export default disallowedProps;
